ac87dc8b906f357e20d9751ed54400d9d4626898
Rey Calantaol <reygcalantaol@gmail.com>
2022-11-02 01:25:37 +0800

    Rename posts ajax content

--- includes/filters.php
+++ includes/filters.php
@@ -9,8 +9,8 @@
 function zior_ajax_page_template( $template ) {
 	if ( isset( $_GET['action'] ) && trim( $_GET['action'] ) === 'filter_posts_widget'
 		&& isset( $_GET['is_ajax'] ) && intval( $_GET['is_ajax'] ) === 1 ) {
-		if ( is_file( ZIOR_PLUGIN_DIR . 'templates/ajax-content.php' ) ) {
-			$template = ZIOR_PLUGIN_DIR . 'templates/ajax-content.php';
+		if ( is_file( ZIOR_PLUGIN_DIR . 'templates/posts-ajax-content.php' ) ) {
+			$template = ZIOR_PLUGIN_DIR . 'templates/posts-ajax-content.php';
 		}
 	}
 	return $template;
