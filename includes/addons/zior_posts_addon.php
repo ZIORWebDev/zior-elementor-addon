0ed408f2a03f62c0a0e95d13598e6a8d9c0c8bd9
Rey Calantaol <reygcalantaol@gmail.com>
2022-11-03 00:55:38 +0800

    Returned proper JSON rather than html

--- includes/addons/zior_posts_addon.php
+++ includes/addons/zior_posts_addon.php
@@ -13,9 +13,11 @@ class ZIOR_Posts_Addon {
 	}
 
 	public function init_hooks() {
-		add_action( 'elementor/frontend/before_render', [ $this, 'before_posts_render' ] );
+		add_action( 'elementor/frontend/before_render', [ $this, 'before_posts_render' ], 99 );
 		add_action( 'elementor/element/posts/section_query/before_section_end', [ $this, 'posts_form_widget_controls' ], 10, 2 );
 		add_action( 'elementor/query/query_results', [ $this, 'query_results_not_found' ], 10, 2 );
+
+		add_action( 'elementor/frontend/after_render', [ $this, 'ajax_response_after' ], 99 );
 	}
 
 	/*
@@ -61,8 +63,7 @@ class ZIOR_Posts_Addon {
 			$settings = $widget->get_settings();
 
 			if ( $settings['show_empty_message'] == 'yes' ) {
-				$empty_message = $settings['custom_empty_message'];
-				echo '<div class="app-posts-not-found">' . wp_kses( $empty_message, wp_kses_allowed_html() )  . '</div>';
+				echo '<div class="app-posts-not-found">' . wp_kses( $settings['custom_empty_message'], wp_kses_allowed_html() )  . '</div>';
 			}
 		}
 	}
@@ -75,39 +76,38 @@ class ZIOR_Posts_Addon {
 	* @return object
 	*/
 	function custom_query_callback( $query ) {
-		$keyword = sanitize_text_field( $_GET['keyword'] );
+		$keyword = sanitize_text_field( $_GET['keyword'] ?? '' );
 		if ( ! empty( $keyword ) ) {
 			$query->query_vars['s'] = $keyword;
 		}
 
-		$year = sanitize_text_field( $_GET['_year'] );
+		$year = sanitize_text_field( $_GET['_year'] ?? '' );
 		if ( is_int( $year ) ) {
 			$query->query_vars['year'] = $year;
 
-			$month = sanitize_text_field( $_GET['month'] );
+			$month = sanitize_text_field( $_GET['month'] ?? '' );
 			if ( is_int( $month ) ) {
 				$query->query_vars['monthnum'] = $month;
 			}
 		}
 		
-		$page_num = absint( sanitize_text_field( $_GET['page_num'] ) );
+		$page_num = absint( sanitize_text_field( $_GET['page_num'] ?? '' ) );
 		$page_num = ( $page_num === 0 ) ? 1 : $page_num;
-		$query->query_vars['paged'] = trim( $_GET['page_num'] );
+		$query->query_vars['paged'] = trim( $page_num );
 
-		$term = get_term( absint( $_GET['term_id'] ) );
-
-		if ( $term ) {
-			$taxonomy = sanitize_title( $_GET['taxonomy'] );
-			$query->query_vars[ $taxonomy ] = $term->slug;
+		$term = get_term( absint( $_GET['term_id'] ?? '' ) );
+		if ( ! is_wp_error( $term ) ) {
+			$taxonomy = sanitize_title( $_GET['taxonomy'] ?? '' );
+			$query->query_vars[ $taxonomy ] = $term->slug ?? '';
 			$query->tax_query->queries[0] = [
 				'taxonomy' => $taxonomy,
-				'terms'    => [ $term->slug ],
+				'terms'    => [ $term->slug ?? '' ],
 				'field'    => 'slug',
 				'operator' => 'IN'
 			];
 
 			$query->tax_query->queried_terms[ $taxonomy ] = [
-				'terms'    => [ $term->slug ],
+				'terms'    => [ $term->slug ?? '' ],
 				'field'    => 'slug'
 			];
 		}
@@ -115,12 +115,26 @@ class ZIOR_Posts_Addon {
 		return $query;
 	}
 
-	function before_posts_render() {
-		$action   = isset( $_GET['action'] ) ? sanitize_text_field( $_GET['action'] ) : '';
-		$query_id = isset( $_GET['target_query_id'] ) ? sanitize_text_field( $_GET['target_query_id'] ) : '';
-		if ( $action === 'filter_posts_widget' && ! empty( $query_id ) ) {
+	public function before_posts_render( $element ) {
+		$action   = sanitize_text_field( $_GET['action'] ?? '' );
+		$query_id = sanitize_text_field( $_GET['target_query_id'] ?? '' );
+		if ( $action == 'filter_posts_widget' && ! empty( $query_id ) ) {
 			add_action( "elementor/query/{$query_id}", [ $this, 'custom_query_callback' ] );
+			ob_end_clean();
+			ob_start();
 		}
 	}
-	
+
+	public function ajax_response_after( $element ) {
+		$action   = sanitize_text_field( $_GET['action'] ?? '' );
+		$query_id = sanitize_text_field( $_GET['target_query_id'] ?? '' );
+		$settings = $element->get_settings_for_display();
+
+		if ( 'posts' === $element->get_name() && $action === 'filter_posts_widget' && $settings['posts_query_id'] === $query_id ) {
+			$output = ob_get_contents();
+			ob_end_clean();
+			wp_send_json_success( $output );
+			exit;
+		}
+	}
 }
