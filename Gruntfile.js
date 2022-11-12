052d1d611dfe4672f8f44b93831ec5a214ec59eb
Rey Calantaol <reygcalantaol@gmail.com>
2022-11-12 23:51:47 +0800

    Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,89 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-	  pkg: grunt.file.readJSON('package.json'),
-	  concat: {
-		options: {
-		  separator: ';'
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			all: {
+				options: {
+					banner: '/*! <%= pkg.title %> - v<%= pkg.version %>\n' +
+						' * <%= pkg.homepage %>\n' +
+						' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
+						' * Licensed GPLv2+' +
+						' */\n'
+				},
+				files: {
+					'assets/js/main.min.js': ['assets/js/src/main.js'],
+					'assets/js/posts-filter.min.js': ['assets/js/src/posts-filter.js'],
+					'assets/js/slider.min.js': ['assets/js/src/slider.js']
+				}
+			}
 		},
-		dist: {
-		  src: ['src/**/*.js'],
-		  dest: 'dist/<%= pkg.name %>.js'
-		}
-	  },
-	  uglify: {
-		options: {
-		  banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+		cssmin: {
+			options: {
+				banner: '/*! <%= pkg.title %> - v<%= pkg.version %>\n' +
+					' * <%= pkg.homepage %>\n' +
+					' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
+					' * Licensed GPLv2+' +
+					' */\n'
+			},
+			minify: {
+				expand: true,
+				cwd: 'assets/css/src',
+				src: ['*.css', '!*.min.css'],
+				dest: 'assets/css/',
+				ext: '.min.css'
+			}
 		},
-		dist: {
-		  files: {
-			'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-		  }
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'assets/js/src/**/*.js'
+			],
+			options: {
+				curly:   true,
+				eqeqeq:  true,
+				immed:   true,
+				latedef: true,
+				newcap:  true,
+				noarg:   true,
+				sub:     true,
+				undef:   true,
+				boss:    true,
+				eqnull:  true,
+				esversion: 6,
+				globals: {
+					jQuery: true,
+					module: true,
+					window: true,
+					elementorFrontend: true,
+					Swiper: true
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['assets/css/**/*.css'],
+				tasks: ['css'],
+				options: {
+					debounceDelay: 500
+				}
+			},
+			scripts: {
+				files: ['assets/js/src/**/*.js'],
+				tasks: ['test', 'js'],
+				options: {
+					debounceDelay: 500
+				}
+			}
 		}
-	  },
-	  qunit: {
-		files: ['test/**/*.html']
-	  },
-	  jshint: {
-		files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-		options: {
-		  // options here to override JSHint defaults
-		  globals: {
-			jQuery: true,
-			console: true,
-			module: true,
-			document: true
-		  }
-		}
-	  },
-	  watch: {
-		files: ['<%= jshint.files %>'],
-		tasks: ['jshint', 'qunit']
-	  }
 	});
-  
+
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-  
-	grunt.registerTask('test', ['jshint', 'qunit']);
-  
-	grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+	grunt.registerTask('test', ['jshint']);
+	grunt.registerTask( 'css', ['cssmin'] );
+	grunt.registerTask( 'js', ['uglify'] );
+	grunt.registerTask( 'default', ['test', 'js', 'css'] );
 };
