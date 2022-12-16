590adda0357b6818c3343f83a45f4c415a9ee2e8
Rey Calantaol <reygcalantaol@gmail.com>
2022-12-16 21:04:32 +0800

    Current Date shortcode

--- includes/shortcodes/shortcodes.php
+++ includes/shortcodes/shortcodes.php
@@ -2,3 +2,30 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+/**
+ * Current date shortcode
+ * 
+ * param @atts array shortcode attributes
+ * return date string
+ */
+function zior_date_shortcode( $atts ) {
+	$atts = shortcode_atts(
+		array(
+			'format' => '',
+		), $atts
+	);
+
+	if ( ! empty( $atts['format'] ) ) {
+		$dateFormat = sanitize_text_field( $atts['format'] );
+	} else {
+		$dateFormat = 'jS F Y';
+	}
+
+	if ( $dateFormat == 'z' ) {
+		return date_i18n( $dateFormat ) + 1;
+	} else {
+		return date_i18n( $dateFormat );
+	}
+}
+add_shortcode( 'zior_current_date', array( $this, 'zior_date_shortcode' ) );
